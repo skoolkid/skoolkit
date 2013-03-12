19b8da298c20ae1eaa63227812aa383d8fdb712a
Richard Dymond <rjdymond@gmail.com>
2013-03-12 20:43:12 -0300

    Add mm2ctl.py and jsw2ctl.py

---
+++ utils/jsw2ctl.py
@@ -0,0 +1,348 @@
+#!/usr/bin/env python
+import sys
+import os
+
+# Use the current development version of SkoolKit
+SKOOLKIT_HOME = os.environ.get('SKOOLKIT_HOME')
+if not SKOOLKIT_HOME:
+    sys.stderr.write('SKOOLKIT_HOME is not set; aborting\n')
+    sys.exit(1)
+if not os.path.isdir(SKOOLKIT_HOME):
+    sys.stderr.write('SKOOLKIT_HOME={0}; directory not found\n'.format(SKOOLKIT_HOME))
+    sys.exit(1)
+sys.path.insert(0, SKOOLKIT_HOME)
+
+from skoolkit.snapshot import get_snapshot
+
+TEMPLATE = """
+;
+; SkoolKit control file for Jet Set Willy.
+;
+; Room descriptions based on reference material from Andrew Broad
+; <http://webspace.webring.com/people/ja/andrewbroad/> and J. G. Harston
+; <http://mdfs.net/Software/JSW/Docs/>.
+;
+; To build the HTML disassembly, create a z80 snapshot of Jet Set Willy named
+; jsw.z80, and run these commands from the top-level SkoolKit directory:
+;   ./sna2skool.py -c examples/jet_set_willy.ctl jsw.z80 > jet_set_willy.skool
+;   ./skool2html.py examples/jet_set_willy.ref
+;
+b 32768 Room buffer
+Z 32768,256
+b 33024 Guardian buffer
+Z 33024,64
+B 33088 Terminator
+z 33089
+w 33280 Screen buffer address lookup table
+W 33280,256,16
+b 33536 Rope animation table
+B 33536,256,16
+c 33792 Start
+b 33824 Current room number
+b 33825 Conveyor data
+b 33841 Triangle UDGs
+D 33841 #UDGTABLE {{ #UDG33841,56(triangle0) | #UDG33849,56(triangle1) | #UDG33857,56(triangle2) | #UDG33865,56(triangle3) }} TABLE#
+t 33873 'AIR'
+t 33876 '+++++ Press ENTER to Start +++++'
+t 33908 '#SPACE(2)JET-SET WILLY by Matthew Smith...'
+B 33942 #CHR169
+T 33943
+T 33980
+T 34024
+T 34050
+T 34100
+t 34132 'Items collected 000 Time 00:00 m'
+t 34164 'GameOver'
+t 34172 '000'
+t 34175 ' 7:00a'
+t 34181 ' 7:00a'
+t 34187 'Enter Code at grid location#SPACE5'
+t 34219 'Sorry, try code at location#SPACE5'
+g 34251 Game status buffer
+B 34251,26,16
+b 34277 WRITETYPER
+B 34277,22,2
+b 34299 Title screen tune data
+B 34299,100,16
+b 34399 In-game tune data
+B 34399,64,16
+c 34463
+c 34499
+c 34620
+c 34762
+c 35211
+c 35245
+c 35841
+c 35914
+c 36147
+c 36203
+c 36288
+c 36307
+c 36564
+c 37056
+c 37310
+c 37819
+c 37841
+c 37974
+c 38026
+c 38046
+c 38064
+c 38098
+c 38137
+c 38196
+c 38276
+c 38298
+c 38344
+c 38430
+c 38455
+c 38504
+c 38528
+c 38545
+c 38562
+c 38622
+z 38680
+b 38912 Attributes for the top two-thirds of the title screen
+B 38912,512,16
+b 39424 Attributes for the bottom third of the screen during gameplay
+B 39424,256,16
+b 39680 {number_keys}
+b 39808 Attributes for the password screen
+B 39808,128,16
+u 39936
+B 39936,64,16
+b 40000 {foot_barrel}
+b 40064 {maria}
+b 40192 {willy}
+b 40448 Password codes
+B 40448,256,16
+z 40704
+b 40960 Guardian definitions
+B 40960,1023,8
+b 41983 Index of first object
+b 41984 Object table
+b 42496 {toilet}
+u 42624
+B 42624,128,16
+Z 42752
+b 43776 {guardians}
+{rooms}
+u 64768 Unused TRS-DOS code
+C 64768
+B 65005,136,16
+T 65141
+C 65147
+B 65203,2
+T 65205
+C 65236
+B 65410,16,8
+C 65426
+Z 65517
+""".lstrip()
+
+def get_udg_table(addr, fname, num=8):
+    macros = ['#UDGARRAY2,56,,2;{0}-{1}-1-16({2}{3})'.format(b, b + 17, fname, (b - addr) // 32) for b in range(addr, addr + 32 * num, 32)]
+    return '#UDGTABLE {{ {0} }} TABLE#'.format(' | '.join(macros))
+
+def get_graphics(address, title, img_fname_prefix, num_sprites=8):
+    lines = [title]
+    udg_table = get_udg_table(address, img_fname_prefix, num_sprites)
+    lines.append('D {0} {1}'.format(address, udg_table))
+    lines.append('B {0},{1},16'.format(address, 32 * num_sprites))
+    return '\n'.join(lines)
+
+def get_guardian_graphics():
+    lines = ['Guardian graphics']
+    for a in range(43776, 49152, 256):
+        udg_table = get_udg_table(a, 'guardian{0}_'.format(a // 256))
+        lines.append('D {0} {1}'.format(a, udg_table))
+        lines.append('B {0},256,16'.format(a))
+    return '\n'.join(lines)
+
+def get_rooms(snapshot):
+    lines = []
+
+    start = 41984 + snapshot[41983]
+    items = {}
+    for a in range(start, 42240):
+        b1 = snapshot[a]
+        b2 = snapshot[a + 256]
+        room_num = b1 & 63
+        x = b2 & 31
+        y = 8 * (b1 >> 7) + b2 // 32
+        items.setdefault(room_num, []).append((x, y))
+
+    rooms = {}
+    rooms_wp = {}
+    for a in range(49152, 64768, 256):
+        room_num = a // 256 - 192
+        room_name = ''.join([chr(b) for b in snapshot[a + 128:a + 160]]).strip()
+        room_name_wp = room_name
+        if room_name.find('  ') > 0:
+            room_name_wp = ''
+            start = 0
+            in_word = False
+            for end, c in enumerate(room_name + ' '):
+                if c == ' ':
+                    if in_word:
+                        room_name_wp += room_name[start:end]
+                        start = end
+                    in_word = False
+                else:
+                    if not in_word:
+                        room_name_wp += '#SPACE({0})'.format(end - start)
+                        start = end
+                    in_word = True
+        rooms[room_num] = room_name
+        rooms_wp[room_num] = room_name_wp
+
+    for a in range(49152, 64768, 256):
+        room = snapshot[a:a + 256]
+        room_num = a // 256 - 192
+        room_name = rooms[room_num]
+        lines.append('b {0} Room {1}: {2}'.format(a, room_num, rooms_wp[room_num]))
+        if a in (50688, 56320, 56576, 59904, 61440):
+            # Rooms with flashing cells
+            room_image = '#ROOM{0}({1}.gif)'.format(a, room_name.lower().replace(' ', '_'))
+        elif a == 61184:
+            # [
+            room_image = '#ROOM{0}(left_square_bracket)'.format(a)
+        else:
+            room_image = '#ROOM{0}'.format(a)
+        lines.append('D {0} #UDGTABLE {{ {1} }} TABLE#'.format(a, room_image))
+        lines.append('D {0} The first 128 bytes define the room layout. Each bit-pair (bits 7 and 6, 5 and 4, 3 and 2, or 1 and 0 of each byte) determines the type of tile (background, floor, wall or nasty) that will be drawn at the corresponding location.'.format(a))
+        lines.append('B {0},128,8 Room layout'.format(a))
+
+        # Room name
+        lines.append('D {0} The next 32 bytes contain the room name.'.format(a + 128))
+        lines.append('T {0},32 Room name'.format(a + 128))
+
+        # Tiles
+        udgs = []
+        for addr, tile_type in ((a + 160, 'background'), (a + 169, 'floor'), (a + 178, 'wall'), (a + 187, 'nasty'), (a + 196, 'ramp'), (a + 205, 'conveyor')):
+            attr = snapshot[addr]
+            if tile_type == 'background':
+                room_paper = attr & 120
+            img_type = ''
+            if attr >= 128:
+                paper = (attr & 56) // 8
+                ink = attr & 7
+                if paper != ink:
+                    udg_bytes = snapshot[addr + 1:addr + 9]
+                    if not all([b == 0 for b in udg_bytes]) and not all([b == 255 for b in udg_bytes]):
+                        # This tile is flashing
+                        img_type = '.gif'
+            udgs.append('#UDG{0},{1}({2}{3:02d}{4})'.format(addr + 1, attr, tile_type, room_num, img_type))
+        tiles_table = '#UDGTABLE { ' + ' | '.join(udgs) + ' } TABLE#'
+        comment = 'The next 54 bytes contain the attributes and graphic data for the tiles used to build the room.'
+        tile_usage = [' (unused)'] * 6
+        for b in snapshot[a:a + 128]:
+            for i in range(4):
+                tile_usage[b & 3] = ''
+                b >>= 2
+        ramp_length = snapshot[a + 221]
+        if ramp_length:
+            tile_usage[4] = ''
+        conveyor_length = snapshot[a + 217]
+        if conveyor_length:
+            tile_usage[5] = ''
+            conveyor_attr = snapshot[a + 205]
+            b = a + 160
+            while b < a + 205 and snapshot[b] != conveyor_attr:
+                b += 1
+            if b < a + 205:
+                comment += ' Note that because of a bug in the game engine, the conveyor tile is not drawn correctly (see the room image above).'
+        lines.append('D {0} {1}'.format(a + 160, comment))
+        lines.append('D {0} {1}'.format(a + 160, tiles_table))
+        lines.append('B {0},9,9 Background{1}'.format(a + 160, tile_usage[0]))
+        lines.append('B {0},9,9 Floor{1}'.format(a + 169, tile_usage[1]))
+        lines.append('B {0},9,9 Wall{1}'.format(a + 178, tile_usage[2]))
+        lines.append('B {0},9,9 Nasty{1}'.format(a + 187, tile_usage[3]))
+        lines.append('B {0},9,9 Ramp{1}'.format(a + 196, tile_usage[4]))
+        lines.append('B {0},9,9 Conveyor{1}'.format(a + 205, tile_usage[5]))
+
+        # Conveyor/ramp direction, location and length
+        lines.append('D {0} The next 8 bytes define the direction, location and length of the conveyor and ramp.'.format(a + 214))
+        conveyor_d, p1, p2 = snapshot[a + 214:a + 217]
+        conveyor_x = p1 & 31
+        conveyor_y = 8 * (p2 & 1) + (p1 & 224) // 32
+        lines.append('B {0},4 Conveyor direction ({1}), location (x={2}, y={3}) and length ({4})'.format(a + 214, 'right' if conveyor_d else 'left', conveyor_x, conveyor_y, conveyor_length))
+        ramp_d, p1, p2 = snapshot[a + 218:a + 221]
+        ramp_x = p1 & 31
+        ramp_y = 8 * (p2 & 1) + (p1 & 224) // 32
+        lines.append('B {0},4 Ramp direction ({1}), location (x={2}, y={3}) and length ({4})'.format(a + 218, 'right' if ramp_d else 'left', ramp_x, ramp_y, ramp_length))
+
+        # Border colour
+        lines.append('D {0} The next byte specifies the border colour.'.format(a + 222))
+        lines.append('B {0} Border colour'.format(a + 222))
+        lines.append('B {0} Unused'.format(a + 223))
+
+        # Object graphic
+        lines.append('D {0} The next 8 bytes define the object graphic.'.format(a + 225))
+        lines.append('D {0} #UDGTABLE {{ #UDG{0},{1}(object{2:02d}) }} TABLE#'.format(a + 225, room_paper + 3, room_num))
+        lines.append('B {0},8,8 Object graphic{1}'.format(a + 225, '' if items.get(room_num) else ' (unused)'))
+
+        # Rooms to the left, to the right, above and below
+        lines.append('D {0} The next 4 bytes specify the rooms to the left, to the right, above and below.'.format(a + 233))
+        room_left, room_right, room_up, room_down = room[233:237]
+        for addr, num, name, desc in (
+            (a + 233, room_left, rooms_wp.get(room_left), 'to the left'),
+            (a + 234, room_right, rooms_wp.get(room_right), 'to the right'),
+            (a + 235, room_up, rooms_wp.get(room_up), 'above'),
+            (a + 236, room_down, rooms_wp.get(room_down), 'below'),
+        ):
+            if name and name != room_name:
+                lines.append('B {0} Room {1} (#R{2}({3}))'.format(addr, desc, 256 * (num + 192), name))
+            elif name:
+                lines.append('B {0} Room {1} ({2})'.format(addr, desc, name))
+            else:
+                lines.append('B {0} Room {1} (none)'.format(addr, desc))
+
+        lines.append('B {0} Unused'.format(a + 237))
+
+        # Guardians
+        start = a + 240
+        end = a + 256
+        for addr in range(start, end, 2):
+            if snapshot[addr] == 255:
+                end = addr
+                break
+        num_guardians = (end - start) // 2
+        if num_guardians:
+            lines.append('D {0} The next {1} bytes define the guardians.'.format(start, num_guardians * 2))
+            addr = start
+            for i in range(num_guardians):
+                lines.append('B {0},2 Guardian {1}'.format(addr, i + 1))
+                addr += 2
+        else:
+            lines.append('D {0} There are no guardians in this room.'.format(start))
+        if num_guardians < 8:
+            lines.append('B {0},1 Terminator'.format(addr))
+            lines.append('B {0},{1},{2} Unused'.format(addr + 1, a + 255 - addr, a + 255 - addr))
+
+    return '\n'.join(lines)
+
+def write_ctl(snapshot):   
+    sys.stdout.write(TEMPLATE.format(
+        number_keys=get_graphics(39680, 'Number key graphics', 'number_key', 4),
+        foot_barrel=get_graphics(40000, 'Foot/barrel graphic data', 'fb', 2),
+        maria=get_graphics(40064, 'Maria sprite graphic data', 'maria', 4),
+        willy=get_graphics(40192, 'Willy sprite graphic data', 'willy'),
+        toilet=get_graphics(42496, 'Toilet graphics', 'toilet', 4),
+        guardians=get_guardian_graphics(),
+        rooms=get_rooms(snapshot)
+    ))
+
+def show_usage():
+    sys.stderr.write("""Usage: {0} jet_set_willy.[sna|z80]
+
+  Generate a control file for Jet Set Willy.
+""".format(os.path.basename(sys.argv[0])))
+    sys.exit(1)
+
+###############################################################################
+# Begin
+###############################################################################
+if len(sys.argv) < 2:
+    show_usage()
+
+write_ctl(get_snapshot(sys.argv[1]))
