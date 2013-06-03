ab3033ad746cadda7ce9873252a68e64112a6642
Richard Dymond <rjdymond@gmail.com>
2013-06-02 22:28:57 -0300

    Fix unit tests in test_skoolhtml.py

--- tests/test_skoolhtml.py
+++ tests/test_skoolhtml.py
@@ -37,6 +37,7 @@ HEADER = """<?xml version="1.0" encoding="utf-8" ?>
     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 <title>{name}: {title}</title>
 <link rel="stylesheet" type="text/css" href="{path}skoolkit.css" />{script}
 </head>
@@ -55,6 +56,7 @@ INDEX_HEADER = """<?xml version="1.0" encoding="utf-8" ?>
     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 <title>{name}: {title}</title>
 <link rel="stylesheet" type="text/css" href="{path}skoolkit.css" />{script}
 </head>
