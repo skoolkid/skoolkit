d37dd3e4d2f4e3ee0fe96e44607090e5e3e1face
Richard Dymond <rjdymond@gmail.com>
2013-08-25 13:30:31 -0300

    Tidy up test_refparser.py

--- tests/test_refparser.py
+++ tests/test_refparser.py
@@ -4,23 +4,7 @@ import unittest
 from skoolkittest import SkoolKitTestCase
 from skoolkit.refparser import RefParser
 
-TEST_HAS_SECTION = """[Section]
-Blah
-"""
-
-TEST_HAS_SECTIONS = """[Prefix:1]
-Foo
-
-[Prefix:2]
-Bar
-"""
-
-TEST_DICTIONARY = """[Section]
-1=Foo
-Blah=Bar
-"""
-
-class SkoolParserTest(SkoolKitTestCase):
+class RefParserTest(SkoolKitTestCase):
     def _get_parser(self, contents):
         reffile = self.write_text_file(contents, suffix='.ref')
         ref_parser = RefParser()
@@ -28,17 +12,28 @@ class SkoolParserTest(SkoolKitTestCase):
         return ref_parser
 
     def test_has_section(self):
-        ref_parser = self._get_parser(TEST_HAS_SECTION)
+        ref_parser = self._get_parser('[Section]\nBlah')
         self.assertTrue(ref_parser.has_section('Section'))
         self.assertFalse(ref_parser.has_section('NonexistentSection'))
 
     def test_has_sections(self):
-        ref_parser = self._get_parser(TEST_HAS_SECTIONS)
+        ref = '\n'.join((
+            '[Prefix:1]',
+            'Foo',
+            '[Prefix:2]',
+            'Bar'
+        ))
+        ref_parser = self._get_parser(ref)
         self.assertTrue(ref_parser.has_sections('Prefix'))
         self.assertFalse(ref_parser.has_sections('NonexistentPrefix'))
 
     def test_get_dictionary(self):
-        ref_parser = self._get_parser(TEST_DICTIONARY)
+        ref = '\n'.join((
+            '[Section]',
+            '1=Foo',
+            'Blah=Bar'
+        ))
+        ref_parser = self._get_parser(ref)
         section_dict = ref_parser.get_dictionary('Section')
         self.assertTrue(1 in section_dict)
         self.assertEqual(section_dict[1], 'Foo')
