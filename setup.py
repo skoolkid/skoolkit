f97c565cf0fdbf5a7cbdf4afac33685358ffd18e
Richard Dymond <rjdymond@gmail.com>
2013-09-01 11:38:26 -0300

    Fix licence classifier in setup.py

--- setup.py
+++ setup.py
@@ -89,7 +89,7 @@ setup(
     scripts=['bin2tap.py', 'skool2asm.py', 'skool2ctl.py', 'skool2html.py', 'skool2sft.py', 'sna2skool.py', 'tap2sna.py'],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
-        'License :: OSI Approved :: GNU General Public License v3 or later',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.2',
