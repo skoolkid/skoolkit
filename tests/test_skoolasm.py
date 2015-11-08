78eebe2f94945001601a154efa86f8b2dbc46672
Richard Dymond <rjdymond@gmail.com>
2015-11-08 10:11:47 -0400

    Test more arithmetic expressions in the #FONT macro in ASM mode

--- tests/test_skoolasm.py
+++ tests/test_skoolasm.py
@@ -168,7 +168,7 @@ class AsmWriterTest(SkoolKitTestCase, CommonSkoolMacroTest):
         self._test_unsupported_macro(writer, '#FONT55584,,,1{1,2}')
         self._test_unsupported_macro(writer, '#FONT:[foo]0,,5')
         self._test_unsupported_macro(writer, '#FONT32768,3,scale=4{x=1,width=26}(chars)')
-        self._test_unsupported_macro(writer, '#FONT(32768+1,10-6,7*8,4/2)(foo)')
+        self._test_unsupported_macro(writer, '#FONT(32768+1, 10-6, (3+4)*8, 4/2){(7+2)**2, width=8*3}(foo)')
 
     def test_macro_html(self):
         writer = self._get_writer()
