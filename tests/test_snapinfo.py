888a7c26f5034f866245f5cefa0a065d1c9524c7
Richard Dymond <rjdymond@gmail.com>
2016-05-25 19:03:31 -0300

    Test snapinfo.py with a 48K SNA snapshot

--- tests/test_snapinfo.py
+++ tests/test_snapinfo.py
@@ -19,6 +19,42 @@ class SnapinfoTest(SkoolKitTestCase):
         with self.assertRaisesRegexp(SkoolKitError, 'Unrecognised snapshot type$'):
             self.run_snapinfo('unknown.snap')
 
+    def test_sna_48k(self):
+        sna = list(range(23))
+        sna[19] = 4 # Interrupts enabled
+        sna.extend((0, 64)) # SP=16384
+        sna.append(1) # IM 1
+        sna.append(2) # BORDER 2
+        sna.extend((0, 128)) # PC=32768
+        sna.extend([0] * 49150)
+        snafile = self.write_bin_file(sna, suffix='.sna')
+        exp_output = [
+            'RAM: 48K',
+            'Interrupts: enabled',
+            'Interrupt mode: 1',
+            'Border: 2',
+            'Registers:',
+            '  PC  32768 8000    SP  16384 4000',
+            '  IX   4625 1211    IY   4111 100F',
+            '  I       0   00    R      20   14',
+            "  B      14   0E    B'      6   06",
+            "  C      13   0D    C'      5   05",
+            "  BC   3597 0E0D    BC'  1541 0605",
+            "  D      12   0C    D'      4   04",
+            "  E      11   0B    E'      3   03",
+            "  DE   3083 0C0B    DE'  1027 0403",
+            "  H      10   0A    H'      2   02",
+            "  L       9   09    L'      1   01",
+            "  HL   2569 0A09    HL'   513 0201",
+            "  A      22   16    A'      8   08",
+            '    SZ5H3PNC           SZ5H3PNC',
+            "  F 00010101        F' 00000111"
+        ]
+
+        output, error = self.run_snapinfo(snafile)
+        self.assertEqual(error, '')
+        self.assertEqual(exp_output, output)
+
     def test_option_V(self):
         for option in ('-V', '--version'):
             output, error = self.run_snapinfo(option, err_lines=True, catch_exit=0)
