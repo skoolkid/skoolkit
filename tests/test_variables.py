23e09f8139baaefc8c981b016afc7fdb1bf4ef47
Philip M. Anderson <weyoun47@gmail.com>
2016-07-21 22:27:38 +0100

    Test a mixture of variable types

--- tests/test_variables.py
+++ tests/test_variables.py
@@ -158,5 +158,76 @@ class VariableListerTest(SkoolKitTestCase):
         exp_output = ['']
         self._test_variables(variables, exp_output)
 
+    def test_mixed_variables(self):
+        variables = [
+            65, 4, 0, 97, 115, 100, 102,                 # a$="asdf"
+            130, 35, 0, 2, 2, 0, 3, 0,                   # dim b(2,3)
+            0, 0, 1, 0, 0,                               # b(1,1)=1
+            0, 255, 255, 255, 0,                         # b(1,2)=-1
+            0, 0, 64, 0, 0,                              # b(1,3)=64
+            0, 0, 128, 0, 0,                             # b(2,1)=128
+            0, 0, 16, 0, 0,                              # b(2,2)=16
+            0, 0, 32, 0, 0,                              # b(2,3)=32
+            172,111,110,103,111,110,229,                 # longone=2.25
+            130,16, 0, 0, 0,                             #
+            195, 8, 0, 1, 5, 0,                          # dim c$(5)
+            119,                                         # c$(1)="w"
+            119,                                         # c$(2)="w"
+            119,                                         # c$(3)="w"
+            119,                                         # c$(4)="w"
+            119,                                         # c$(5)="w"
+            228,                                         # FOR control variable of name "d"
+            0, 0, 1, 0, 0,                               # 1
+            0, 0, 8, 0, 0,                               # limit = 8
+            0, 0, 1, 0, 0,                               # step = 1
+            4, 0,                                        # line = 4
+            7,                                           # statement = 7
+            101, 0, 0, 250, 255, 0,                      # e=65530
+            0, 12, 3, 0, 243, 105, 13,                   # 12 NEXT i
+            39, 7, 6, 0, 234, 116, 101, 115, 116, 13,    # 9991 REM test
+            70, 5, 0, 122, 120, 99, 118, 98,             # f$="zxcvb"
+            135, 33, 0, 1, 6, 0,                         # dim g(6)
+            0, 0, 12, 0, 0,                              # g(1)=12
+            0, 0, 13, 0, 0,                              # g(2)=13
+            0, 0, 14, 0, 0,                              # g(3)=14
+            0, 0, 15, 0, 0,                              # g(4)=15
+            0, 0, 16, 0, 0,                              # g(5)=16
+            0, 0, 17, 0, 0,                              # g(6)=17
+            172, 111, 110, 103, 101, 114, 111, 110, 229, # longerone=-8.25
+            132, 132, 0, 0, 0,                           #
+            200, 11, 0, 2, 3, 0, 2, 0,                   # dim h$(3,2)
+            97,                                          # h$(1,1)="a"
+            122,                                         # h$(1,2)="z"
+            98,                                          # h$(2,1)="b"
+            121,                                         # h$(2,2)="y"
+            99,                                          # h$(3,1)="c"
+            120,                                         # h$(3,2)="x"
+            233,                                         # FOR control variable of name "i"
+            0, 0, 0, 1, 0,                               # 256
+            0, 0, 0, 2, 0,                               # limit = 512
+            0, 0, 0, 1, 0,                               # step = 256
+            17, 0,                                       # line = 17
+            1,                                           # statement = 1
+            106, 149, 244, 36, 0, 0,                     # j=-2e6
+            0, 10, 3, 0, 245, 112, 13,                   # 10 PRINT p
+            39, 15, 2, 0, 230, 13,                       # 9999 NEW
+            128                                          # End of variables area
+        ]
+        exp_output = [
+            '(String) a$="asdf"',
+            '(Number array) b(2,3)=[1,-1,64,128,16,32]',
+            '(Number) longone=2.25',
+            '(Character array) c$(5)=["w","w","w","w","w"]',
+            '(FOR control variable) d=1 (limit=8, step=1, line=4, statement=7)',
+            '(Number) e=65530',
+            '(String) f$="zxcvb"',
+            '(Number array) g(6)=[12,13,14,15,16,17]',
+            '(Number) longerone=-8.25',
+            '(Character array) h$(3,2)=["a","z","b","y","c","x"]',
+            '(FOR control variable) i=256 (limit=512, step=256, line=17, statement=1)',
+            '(Number) j=-2000000.0',
+        ]
+        self._test_variables(variables, exp_output)
+
 if __name__ == '__main__':
     unittest.main()
