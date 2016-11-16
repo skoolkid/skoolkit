f71c93f9daf4f8acfe07bdbe1e747b7081753d8b
Richard Dymond <rjdymond@gmail.com>
2016-11-16 18:29:58 -0400

    Migrate tools/check-links.py to ArgumentParser

--- tools/check-links.py
+++ tools/check-links.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import sys
 import os
+import argparse
 
 SKOOLKIT_HOME = os.environ.get('SKOOLKIT_HOME')
 if not SKOOLKIT_HOME:
@@ -14,81 +15,69 @@ sys.path.insert(0, SKOOLKIT_TESTS)
 
 from disassemblytest import check_links
 
-def parse_args(args):
-    show_counts = False
-    show_links = False
-    p_args = []
-    i = 0
-    while i < len(args):
-        arg = args[i]
-        if arg == '-c':
-            show_counts = True
-        elif arg == '-s':
-            show_links = True
-        elif arg.startswith('-'):
-            usage()
-        else:
-            p_args.append(arg)
-        i += 1
-    if len(p_args) != 1:
-        usage()
-    return show_counts, show_links, p_args[0]
+def run(root_dir, options):
+    all_files, orphans, missing_files, missing_anchors = check_links(root_dir)
 
-def usage():
-    sys.stderr.write("""Usage: {} [options] ROOTDIR
+    if options.counts:
+        html = img = css = js = 0
+        other_files = []
+        for fname in all_files:
+            if fname.endswith('.html'):
+                html += 1
+            elif fname.endswith(('.png', '.gif')):
+                img += 1
+            elif fname.endswith('.css'):
+                css += 1
+            elif fname.endswith('.js'):
+                js += 1
+            else:
+                other_files.append(fname)
+        print('HTML files: {}'.format(html))
+        print('Image files: {}'.format(img))
+        print('CSS files: {}'.format(css))
+        print('JavaScript files: {}'.format(js))
+        print('Other files: {}'.format(len(other_files)))
+        for fname in other_files:
+            print('  {}'.format(fname))
 
-  Checks links (to both files and anchors) in all HTML files under ROOTDIR.
+    if options.links:
+        for fname in all_files:
+            if fname.endswith('.html'):
+                links = all_files[fname][1]
+                if links:
+                    print('{}:'.format(fname))
+                    for link_dest in links:
+                        print('  -> {}'.format(link_dest))
 
-Available options:
-  -c  Show file counts
-  -s  Show links
-""".format(os.path.basename(sys.argv[0])))
-    sys.exit(1)
-
-show_counts, show_links, root_dir = parse_args(sys.argv[1:])
-all_files, orphans, missing_files, missing_anchors = check_links(root_dir)
-
-if show_counts:
-    html = img = css = js = 0
-    other_files = []
-    for fname in all_files:
-        if fname.endswith('.html'):
-            html += 1
-        elif fname.endswith(('.png', '.gif')):
-            img += 1
-        elif fname.endswith('.css'):
-            css += 1
-        elif fname.endswith('.js'):
-            js += 1
-        else:
-            other_files.append(fname)
-    print('HTML files: {}'.format(html))
-    print('Image files: {}'.format(img))
-    print('CSS files: {}'.format(css))
-    print('JavaScript files: {}'.format(js))
-    print('Other files: {}'.format(len(other_files)))
-    for fname in other_files:
+    print('Orphaned files: {}'.format(len(orphans)))
+    for fname in orphans:
         print('  {}'.format(fname))
 
-if show_links:
-    for fname in all_files:
-        if fname.endswith('.html'):
-            links = all_files[fname][1]
-            if links:
-                print('{}:'.format(fname))
-                for link_dest in links:
-                    print('  -> {}'.format(link_dest))
+    print('Links to non-existent files: {}'.format(len(missing_files)))
+    for fname, link_dest in missing_files:
+        print('  {} -> {}'.format(fname, link_dest))
 
-print('Orphaned files: {}'.format(len(orphans)))
-for fname in orphans:
-    print('  {}'.format(fname))
+    print('Links to non-existent anchors: {}'.format(len(missing_anchors)))
+    for fname, link_dest in missing_anchors:
+        print('  {} -> {}'.format(fname, link_dest))
 
-print('Links to non-existent files: {}'.format(len(missing_files)))
-for fname, link_dest in missing_files:
-    print('  {} -> {}'.format(fname, link_dest))
+    return 1 if orphans or missing_files or missing_anchors else 0
 
-print('Links to non-existent anchors: {}'.format(len(missing_anchors)))
-for fname, link_dest in missing_anchors:
-    print('  {} -> {}'.format(fname, link_dest))
-
-sys.exit(1 if orphans or missing_files or missing_anchors else 0)
+###############################################################################
+# Begin
+###############################################################################
+parser = argparse.ArgumentParser(
+    usage='check-links.py [options] ROOTDIR',
+    description="Check links (to both files and anchors) in all HTML files under ROOTDIR.",
+    add_help=False
+)
+parser.add_argument('root_dir', help=argparse.SUPPRESS, nargs='?')
+group = parser.add_argument_group('Options')
+group.add_argument('-c', dest='counts', action='store_true',
+                   help='Show file counts')
+group.add_argument('-s', dest='links', action='store_true',
+                   help='Show links')
+namespace, unknown_args = parser.parse_known_args()
+if unknown_args or namespace.root_dir is None:
+    parser.exit(2, parser.format_help())
+sys.exit(run(namespace.root_dir, namespace))
