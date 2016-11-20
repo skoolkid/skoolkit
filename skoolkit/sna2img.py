95079f7ed4d115b9967d8ba9a7ca993e197d658e
Richard Dymond <rjdymond@gmail.com>
2016-11-20 08:10:45 -0400

    Remove unused import

--- skoolkit/sna2img.py
+++ skoolkit/sna2img.py
@@ -16,7 +16,6 @@
 # You should have received a copy of the GNU General Public License along with
 # SkoolKit. If not, see <http://www.gnu.org/licenses/>.
 
-import sys
 import os
 import argparse
 try:
