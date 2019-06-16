3f16de053ab5cd7513cb7bf066d529dd70a20f8e
Richard Dymond <rjdymond@gmail.com>
2019-06-16 16:04:13 -0300

    Restore body_max_width option for the Sphinx classic theme

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -91,12 +91,14 @@ highlight_language = 'none'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'classic'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    'body_max_width': 'none'
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
