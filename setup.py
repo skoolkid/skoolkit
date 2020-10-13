ff8e7547c0c4a921c736d3b8695962a78c181905
Richard Dymond <rjdymond@gmail.com>
2020-10-13 17:08:59 -0300

    Add support for Python 3.9

--- setup.py
+++ setup.py
@@ -153,6 +153,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Topic :: Software Development :: Disassemblers',
         'Topic :: Utilities'
     ]
