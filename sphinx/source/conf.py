d1aa73b9240aa0bd3d4fa7001d43d1bc1624efc5
Richard Dymond <rjdymond@gmail.com>
2021-09-09 17:26:14 -0300

    Add custom CSS to left-align tables in the Sphinx documentation
    In version 2.1.0, Sphinx switched to centring tables by default, but
    provided no configuration option to revert to the original behaviour.

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -124,6 +124,8 @@ html_theme_options = {
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_css_files = ['custom.css']
+
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
 #html_last_updated_fmt = '%b %d, %Y'
