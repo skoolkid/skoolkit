273c6cd34cf59e2584db74d3b0d3110d9e4ccba2
Richard Dymond <rjdymond@gmail.com>
2021-10-07 16:48:35 -0300

    Add support for Python 3.10

--- setup.py
+++ setup.py
@@ -150,6 +150,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Topic :: Software Development :: Disassemblers',
         'Topic :: Utilities'
     ]
