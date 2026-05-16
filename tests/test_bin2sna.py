c18a187e5ebf19df15f59031b3e811b64b2fed52
Richard Dymond <rjdymond@gmail.com>
2026-05-16 07:46:50 -0300

    Fix failing test for Python 3.14.5

--- tests/test_bin2sna.py
+++ tests/test_bin2sna.py
@@ -263,7 +263,7 @@ class Bin2SnaTest(SkoolKitTestCase):
 
     def test_option_page_invalid(self):
         for option, exp_error in (
-                ('--page 8', r"invalid choice: '?8'? \(choose from 0, 1, 2, 3, 4, 5, 6, 7\)$"),
+                ('--page 8', r"invalid choice: '?8'? \(choose from '?0'?, '?1'?, '?2'?, '?3'?, '?4'?, '?5'?, '?6'?, '?7'?\)$"),
                 ('--page x', "invalid int value: 'x'$")
         ):
             output, error = self.run_bin2sna(f'{option} in.bin', catch_exit=2)
